96234ce246e14b7e5a875035d6b99dc7071cb1c3
‘zkj’ <zkj871757929@qq.com>
2021-11-21 10:45:38 +0800

    dsd

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 
 int main() {
     printf("Hello, World!\n");
-    std::cout<<"lin"<<std::endl;
+    std::cout<<"lin"dsjgusd<<std::endl;
     std::cout<<"lindadada"<<std::endl;
     return 0;
-ahduisah
-ikajfjoiasdfj}
+;ahduisah
+ik
+asdkasod
+asdjas
+asdksjdajfjoiasdfj}:
